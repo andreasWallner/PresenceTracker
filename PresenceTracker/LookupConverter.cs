a10c6b5193823abcb8a2ac68623e39d1711e0f94
Andreas Wallner <A.Wallner@innovative-solutions.at>
2014-06-11 00:14:19 +0200

    replaced ugly color converter with generic XAML driven approach

---
+++ PresenceTracker/LookupConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace PresenceTracker
+{
+    [ValueConversion(typeof(object), typeof(object))]
+    public class LookupConverter : MarkupExtension, IValueConverter
+    {
+        public LookupConverter() { }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null)
+                return DependencyProperty.UnsetValue;
+
+            SortedList l = parameter as SortedList;
+            if (l == null)
+                return DependencyProperty.UnsetValue;
+
+            if (value == null)
+                return null;
+
+            if (!l.ContainsKey(value))
+                return DependencyProperty.UnsetValue;
+
+            return l[value];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PresenceTracker/State.cs
+++ PresenceTracker/State.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
 using System.Windows.Media;
@@ -20,49 +21,5 @@ namespace PresenceTracker
         AppStart,
         Resume,
         Suspend
-    }
-
-    [ValueConversion(typeof(State), typeof(SolidColorBrush))]
-    public class StateToColorConverter : MarkupExtension, IValueConverter
-    {
-        public StateToColorConverter() { }
-
-        public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            return this;
-        }
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-
-            if (value != null && value is State)
-            {
-                switch ((State)value)
-                {
-                    case State.AppStart:
-                        return new SolidColorBrush(Colors.ForestGreen);
-                    case State.Lock:
-                        return new SolidColorBrush(Colors.Yellow);
-                    case State.Logoff:
-                        return new SolidColorBrush(Colors.Red);
-                    case State.Logon:
-                        return new SolidColorBrush(Colors.Green);
-                    case State.Shutdown:
-                        return new SolidColorBrush(Colors.Red);
-                    case State.Unknown:
-                        return new SolidColorBrush(Colors.SaddleBrown);
-                    case State.Unlock:
-                        return new SolidColorBrush(Colors.Green);
-                    default:
-                        return new SolidColorBrush(Colors.Transparent);
-                }
-            }
-            return null;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
